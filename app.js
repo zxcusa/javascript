ea470eb7f23bb80bbbd8eb2995626485a31af188
zxcusa <zxcusa0208@gmail.com>
2026-05-01 10:13:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const S = 1000;
+const p = 10;
+
+let summa = S;
+let k = 0;
+
+while (summa <=2 * S) {
+    summa += summa * p / 100;
+    k++;
+}
+
+console.log("Oylar soni:", k);
+console.log("Bankdagi summa:", summa);
